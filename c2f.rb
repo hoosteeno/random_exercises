efb6d1abe1b3802f263a9e12eff0bd07ebe76c56
hoosteeno <justin@tentacu.com>
2015-09-23 08:40:35 -0600

    fix math problem! add 'unit test'

--- c2f.rb
+++ c2f.rb
@@ -1,31 +1,46 @@
-def convert_c(c)
-  temp = ((c + 32) * 9 / 5).round(2)
-  return "#{temp}F"
+def from_c (deg)
+  return "#{(deg * 9 / 5 + 32).round(2)}F"
 end
 
-def convert_f(f)
-  temp = ((f - 32) * 5 / 9).round(2)
-  return "#{temp}C"
+def from_f (deg)
+  return "#{((deg - 32) * 5 / 9).round(2)}C"
+end
+
+def test()
+  f = from_c(100.0)
+  c = from_f(212.0)
+  unless (c == "100.0C" and f == "212.0F")
+    return "failed: There is a math problem."
+  end
+  return "success!"
 end
 
 def usage
-  puts "You must enter a temperature and a scale, e.g. 46F or 18c"
+  puts "You must enter a temperature to be converted, e.g. 46F or 18c"
   exit
 end
 
-def handle(string)
-  if (m = /^(\d+)([fc])$/i.match(string))
-    temp = m[1]
-    fn = "convert_#{m[2].downcase}"
-    return send(fn, temp.to_f)
-  elsif (m = /^(\d+)$/.match(string))
-    return convert_c(m[1].to_f)
+def handle(input)
+
+  # if temp+scale, e.g. 99f, convert
+  if (m = /^(\d+)([fc])$/i.match(input))
+    temp = m[1].to_f
+    initial_scale = m[2]
+    if (initial_scale == "F")
+      return from_f(temp)
+    else
+      return from_c(temp)
+    end
+
+  elsif (input == "TEST")
+    return test()
+
   else
     usage
   end
+
 end
 
 print "Enter a temperature to convert (e.g. 25C or 93F): "
-input = gets.chomp
-#TODO: don't output the random chars people put in, like "43dfdai"
-puts "#{input.upcase} is #{handle(input)}"
+input = gets.chomp.upcase
+puts "#{input} is #{handle(input)}"
